9f98c7cf7dec3ad9d781395f3718d249b5700daf
the-ho-mang <anhmt@twendeesoft.com>
2024-03-15 23:09:02 +0700

    add global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix("nft-adv");
+
   const config = new DocumentBuilder()
     .setTitle("none")
     .setVersion("1.0")
